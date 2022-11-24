bed46aef7cc1a051ce8aa14eaa0012a1afa38f8f
Donny <112881062+D0N09@users.noreply.github.com>
2022-11-24 18:02:35 +0100

    Add files via upload

---
+++ js/animation.js
@@ -0,0 +1,9 @@
+console.log("hallo");
+
+
+function deuropenen() {
+    console.log("je klikt op de deur");
+    alert ("De deur is op slot!")
+}
+
+document.getElementById("door").addEventListener("click", deuropenen);
